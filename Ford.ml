d41cfb8f5b1056a9d9ff6646f8c6ac080cdf4202
PierreBinet <pbinet@etud.insa-toulouse.fr>
2018-11-19 17:25:19 +0100

    partage ressources

--- Ford.ml
+++ Ford.ml
@@ -3,14 +3,12 @@ let convFlow x = (0, int_of_string x);;
 
 let rec find_path gr source target pathlist =
   let rec findSource gr source = match gr with
-    |[]-> raise Not_found
-    |(source,outarc)::rest->outarc
-    |(other,_)::rest->(findSource rest source)
+    |[]->raise Not_found
+    |(x,outarc)::rest->(if (x==source) then outarc else (findSource rest source))
   in
-    match (findSource gr source)  with
-      |[]->raise Not_found
-      |(target,_)::rest-> (List.append pathlist [target])
-      |(other,_)::rest-> (List.append pathlist (find_path gr other target pathlist))
+    match (findSource gr source) with
+      |[]->None
+      |(x,_)::rest-> if (x==target) then (List.append pathlist [Some x]) else (List.append pathlist (find_path gr x target pathlist))
 
 
 
--- ftest.ml
+++ ftest.ml
@@ -9,16 +9,15 @@ map testgraph2 f;;
 
 if Array.length Sys.argv <> 5 then
   begin
-    Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
+    Printf.printf "\nUsage: %s infile source target outfile\n\n%!" Sys.argv.(0) ;
     exit 0
   end ;
 
 let infile = Sys.argv.(1)
 and outfile = Sys.argv.(4)
-  
-(* These command-line arguments are not used for the moment. *)
-and _source = Sys.argv.(2)
-and _sink = Sys.argv.(3)
+
+and source = Sys.argv.(2)
+and target = Sys.argv.(3)
 in
 
 (* Open file *)
