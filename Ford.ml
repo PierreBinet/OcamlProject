e5e1c12048d23a3b0d677e9ad7ca3ee77a79344d
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2018-11-19 17:04:05 +0100

    algo ford fulkerson

--- Ford.ml
+++ Ford.ml
@@ -1,29 +1,16 @@
 let convFlow x = (0, int_of_string x);;
 
-let rec indiceNode gr n= match gr with
-  |[]-> raise Not_found
-  |(n,_)::rest->0
-  |_::rest-> 1+(indiceNode rest n)
-
-;;
-
-
-let findSource gr = nth gr (indiceNode (find (let f x = (x==source)) gr))
-;;
-
-nth
 
 let rec find_path gr source target pathlist =
-  match gr with
-    |[]->None
-    |(source,out_arc_src)::(target,_)::[]->(let rec aux1 out_arc _src = match out_arc _src with
-                                              |[]->None
-                                              |(target,_)::rest->Some [target]
-                                              |_-> aux1 rest )
-    |(source,out_arc_src)::rest->
-    |_->
-
-
+  let rec findSource gr source = match gr with
+    |[]-> raise Not_found
+    |(source,outarc)::rest->outarc
+    |(other,_)::rest->(findSource rest source)
+  in
+    match (findSource gr source)  with
+      |[]->raise Not_found
+      |(target,_)::rest-> (List.append pathlist [target])
+      |(other,_)::rest-> (List.append pathlist (find_path gr other target pathlist))
 
 
 
