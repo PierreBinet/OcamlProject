2b5e6e41d9eacb1be1e07027543dd7be3ae987bf
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2018-11-27 10:42:35 +0100

    ajout dest dans le path retourné par find_path

--- Ford.ml
+++ Ford.ml
@@ -4,15 +4,15 @@ open Gfile
 let convFlow x = (0, int_of_string x);;
 
 let rec find_path (gr: int graph) (visited: id list) (id1:id) (id2:id) =
-let rec path_temp = function
-|[]-> List.append visited []
-|(x,_)::rest-> 
-if (x == id2)
-then (List.append visited [id1])
-else if (List.mem x visited)
-then 
-path_temp rest
-else 
-(find_path gr (List.append visited [id1]) x id2)
-in
-path_temp (out_arcs gr id1);;
+  let rec path_temp = function
+    |[]-> List.append visited [id2]
+    |(x,_)::rest-> 
+        if (x == id2)
+        then (List.append visited [id1])
+        else if (List.mem x visited)
+        then 
+          path_temp rest
+        else 
+          (find_path gr (List.append visited [id1]) x id2)
+  in
+    path_temp (out_arcs gr id1);;
